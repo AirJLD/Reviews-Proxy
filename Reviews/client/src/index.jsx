96c0db490ec28eefc9a8da07b42b0c120605595f
qulingyi <lingyiququ@gmail.com>
2018-11-08 17:45:51 -0800

    proxy set up done

--- Reviews/client/src/index.jsx
+++ Reviews/client/src/index.jsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 
 import App from './app.jsx';
 
-//ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<App />, document.getElementById('reviews'));
 window.Reviews = App;
--- proxy/server.js
+++ proxy/server.js
@@ -9,8 +9,22 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+const proxy = require('http-proxy-middleware');
+
+const options = {
+  target: 'http://localhost:3000',
+  changeOrigin: true,
+  router: {
+    '/api': 'http://localhost:3001',
+  },
+};
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const apiProxy = proxy(options);
+
+app.use('/api', apiProxy);
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
