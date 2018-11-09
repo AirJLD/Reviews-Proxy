d998cb1fb0e5ea42c8de66b4031af35dbc274722
qulingyi <lingyiququ@gmail.com>
2018-11-09 15:06:04 -0800

    .gitignore fix

--- Booking/client/index.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-// import { render } from 'react-dom';
-import Booking from './components/booking';
-
-// render(<Booking />, document.getElementById('booking'));
-
-window.Booking = Booking;
--- Booking/server/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./api');
-
-const port = 3002;
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
